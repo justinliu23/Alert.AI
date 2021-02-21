a31c3fca559558b85012f58a813293c4a637ea11
Marshall Vielmetti <mvielmetti@outlook.com>
2021-02-21 14:00:49 -0600

    Reactified some pages + code gen working

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import {
   BrowserRouter as Router,
   Switch,
   Route,
 } from "react-router-dom";
 import HomePage from './Pages/Homepage'
-import NewUserPage from './Pages/NewUserPage'
+import UserPage from './Pages/UserPage'
 import SignInPage from './Pages/SignInPage'
 import StudentPage from './Pages/StudentPage'
 import TeacherPage from './Pages/TeacherPage'
+import TeacherHome from './Pages/TeacherHome'
 import PrivateRoute from './Components/PrivateRoute/PrivateRoute'
 
 //  import Login from './Components/Login/Login'
 // import PrivateRoute from './Components/PrivateRoute/PrivateRoute'
 
+function makeid(length) {
+  var result           = '';
+  var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var charactersLength = characters.length;
+  for ( var i = 0; i < length; i++ ) {
+     result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
+
 function App() {
+  
   return (
     <Router>
       <div>
@@ -25,14 +37,23 @@ function App() {
             <SignInPage />
           </Route>
           <Route exact path="/sign-up">
-            <NewUserPage />
+            <UserPage />
           </Route>
           <Route exact path="/student">
             <StudentPage />
           </Route>
-          <Route exact path="/teacher">
-            <TeacherPage />
+          <Route exact path="/teacherhome">
+            <TeacherHome />
           </Route>
+          <Route
+            exact path="/teacher"
+            render={(props) => (
+              <TeacherPage
+                {...props}
+                classCode={makeid(6)}
+              />
+            )}
+          />
           <Route exact path="/">
             <HomePage />
           </Route>
