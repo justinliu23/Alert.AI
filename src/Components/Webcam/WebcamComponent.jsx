bbd909636b8441b92c907ec7931c2385e4a58825
Marshall Vielmetti <mvielmetti@outlook.com>
2021-02-21 07:54:07 -0600

    added page structure and webcam

---
+++ src/Components/Webcam/WebcamComponent.jsx
@@ -0,0 +1,70 @@
+import React, {useState, useRef, useCallback, useEffect} from "react";
+import Webcam from 'react-webcam'
+import './WebcamComponent.css'
+
+const videoConstraints = {
+  width: 1280,
+  height: 720,
+  facingMode: "user",
+};
+
+const WebcamComponent = () => {
+  const webcamRef = useRef(null);
+  const [imgSrc, setImgSrc] = useState(null);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      //updates imgSrc
+      capture()
+
+      const imgData = new FormData();
+      const data = {
+        'imgStr': imgSrc,
+      }
+      console.log(imgSrc)
+
+      fetch('http://127.0.0.1:5000/api/images', {
+        method: 'POST',
+        mode: 'no-cors',
+        cache: 'no-cache',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data)
+      }).then((err) => console.log(err))
+
+    }, 5000);
+    return () => clearInterval(interval);
+  });
+
+  const capture = useCallback(() => {
+    const imageSrc = webcamRef.current.getScreenshot();
+    setImgSrc(imageSrc);
+
+    
+  }, [webcamRef, setImgSrc]);
+
+  return (
+    <>
+    <div className='border'>
+      <Webcam
+        audio={false}
+        ref={webcamRef}
+        screenshotFormat="image/jpeg"
+        width={1280}
+        videoConstraints={videoConstraints}
+      />
+    </div>
+    <button onClick={capture}>Capture photo</button>
+    {imgSrc && (
+        <img
+          src={imgSrc}
+          alt=''
+        />
+      )}
+    </>
+  );
+};
+
+
+export default WebcamComponent;
