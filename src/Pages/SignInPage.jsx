3346f9dc3bbd5562c47bfd7008775eda1fcab4a4
Marshall Vielmetti <mvielmetti@outlook.com>
2021-02-21 15:45:15 -0600

    added sign in redirect

--- src/Pages/SignInPage.jsx
+++ src/Pages/SignInPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import Navbar from "../Components/Navbar/Navbar";
-import { Link } from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
 import "./signintemplate.css";
 
 export default function SignInPage(props) {
@@ -10,8 +10,15 @@ export default function SignInPage(props) {
   const handleClick = () => setIsTeacher(!isTeacher);
 
   const handleSignIn = (e) => {
-    e.preventDefault() 
-    props.setUsername(userName)
+    e.preventDefault();
+    props.setUsername(userName);
+    if (userName !== "") {
+      if (isTeacher) {
+        props.history.push(`/teacherhome`);
+      } else {
+        props.history.push(`/student`);
+      }
+    }
 
     //WIP CODE FOR AUTHENTICATION
     // e.preventDefault() ;
@@ -27,7 +34,7 @@ export default function SignInPage(props) {
     // }).then( res => res.json())
     // .then(data=>{
     //   localStorage.setItem('access_token', data.access_token);
-      
+
     //   localStorage.setItem('username', data.username);
 
     //   if (localStorage.getItem("access_token") !== null && localStorage.getItem("access_token")!=="undefined") {
@@ -36,7 +43,7 @@ export default function SignInPage(props) {
     //       alert(data.error);
     //   }
     // }).catch(err => console.log(err));
-  }
+  };
 
   return (
     <>
@@ -45,33 +52,35 @@ export default function SignInPage(props) {
         <h1>Sign In</h1>
         <form>
           <fieldset>
-          <input
-            type="username"
-            placeholder="Username"
-            required
-            value={userName}
-            onChange={(e) => setUserName(e.target.value)}
-          />
-          <input
-            type="password"
-            placeholder="Password"
-            required
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <label>
-            Are you a teacher?
             <input
-              type="checkbox"
-              onClick={(e) => handleClick(e)}
-              checked={isTeacher}
+              type="username"
+              placeholder="Username"
+              required
+              value={userName}
+              onChange={(e) => setUserName(e.target.value)}
             />
-            <span class="checkmark" />
-          </label>
-          <div>
-            <button className="next action-button" onClick={handleSignIn}>Sign In</button>
-            <Link to="/sign-up">Sign Up</Link>
-          </div>
+            <input
+              type="password"
+              placeholder="Password"
+              required
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <label>
+              Are you a teacher?
+              <input
+                type="checkbox"
+                onClick={(e) => handleClick(e)}
+                checked={isTeacher}
+              />
+              <span class="checkmark" />
+            </label>
+            <div>
+              <button className="next action-button" onClick={handleSignIn}>
+                Sign In
+              </button>
+              <Link to="/sign-up">Sign Up</Link>
+            </div>
           </fieldset>
         </form>
       </div>
