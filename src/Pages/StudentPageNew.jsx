db9c23f4eaa89b8209b978ec40a7984994c8762a
mishcoder <30580670+mishcoder@users.noreply.github.com>
2021-02-21 11:08:28 -0800

    Add files via upload

---
+++ src/Pages/StudentPageNew.jsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import Navbar from "../Components/Navbar/Navbar";
+import "./StudentPage.css";
+import "./Modal.js";
+
+export default function StudentPage() {
+    return (
+    <>
+    <Navbar />
+        <section class="testimonials" id="testimonials">
+            <div class = "title">
+              <h2 class ="titleText"> Classes </h2>
+              <button id="myBtn">Join Class</button>
+
+        <!-- The Modal -->
+        <div id="myModal" class="modal">
+
+          <!-- Modal content -->
+          <div class="modal-content">
+            <span class="close">&times;</span>
+            <p>Enter Code:</p>
+            <input type = "text"><br/>
+            <input type = "submit" placeholder="Submit" class = "submitButton">
+          </div>
+
+        </div>
+            </div>
+            <div class ="content">
+              <div class="box">
+                <div class = "imgBx">
+                  <img src="https://friendlystock.com/wp-content/uploads/2020/06/9-old-calculator-cartoon-clipart.jpg">
+                </div>
+                <div class = "text">
+                  <p>Math</p>
+                  <h3>Teacher</h3>
+                </div>
+              </div>
+              <div class="box">
+                <div class = "imgBx">
+                  <img src="https://i.pinimg.com/736x/1b/4c/60/1b4c60f47895a0928f2567dec949d4a3.jpg">
+                </div>
+                <div class = "text">
+                  <p>English</p>
+                  <h3>Teacher</h3>
+                </div>
+              </div>
+              <div class="box">
+                <div class = "imgBx">
+                  <img src="https://rlv.zcache.com/weird_science_jug_cutout-r9aa46d96d3f84559bdf351370b98ecea_x7saw_8byvr_307.jpg">
+                </div>
+                <div class = "text">
+                  <p>Science</p>
+                  <h3>Teacher</h3>
+                </div>
+              </div>
+            </div>
+          </section>
+    )
+}
