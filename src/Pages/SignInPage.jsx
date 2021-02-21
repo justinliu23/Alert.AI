73648f26dc4ec3efb1b5b35fefc1df443446e233
Marshall Vielmetti <mvielmetti@outlook.com>
2021-02-21 08:46:36 -0600

    updated backend

--- src/Pages/SignInPage.jsx
+++ src/Pages/SignInPage.jsx
@@ -1,11 +1,28 @@
 import React from 'react'
 import Navbar from '../Components/Navbar/Navbar'
+import './signpages.css'
 
 export default function SignInPage() {
     return (
         <div>
             <Navbar />
-            SignIn
+            <section className="signin">
+			<div className = "centered">
+				<h1>Sign Up</h1>
+				<form>
+					<input type = "username" placeholder="Username" required>
+					<input type = "password" placeholder="Password" required>
+                    <label className="container">Are you a teacher?
+                          <input type="checkbox" checked="checked">
+                          <span className="checkmark"></span>
+                        </label>
+					<div>
+						<button>Sign up</button>
+						<a href = "/sign-in">Login.</a>
+					</div>
+				</form>
+			    </div>
+		    </section>
         </div>
     )
 }
